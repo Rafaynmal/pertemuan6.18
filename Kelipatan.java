d4c14b64c914ef4d23eaa85f8c6cabd6f391c86f
Rafaynmal <rafihayyan16@gmail.com>
2024-11-04 21:29:41 +0700

    Add files via upload

---
+++ Kelipatan.java
@@ -0,0 +1,18 @@
+package pertemuan6;
+
+/**
+ *
+ * @author Afra Syavina
+ */
+public class Kelipatan {
+    public static void main(String[] args) {
+        double start = 3.5;
+        double end = 35.0;
+        double increment = 3.5;
+
+        for (double i = start; i <= end; i += increment) {
+            System.out.println(i);
+        }
+        System.out.println("BUILD SUCCESSFUL");
+    }
+}
